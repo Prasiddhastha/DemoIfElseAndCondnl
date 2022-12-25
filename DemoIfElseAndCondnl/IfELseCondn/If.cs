da411fe54aeed0b991eb356b9da25d34c03a7204
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2022-12-25 16:31:06 +0545

    Add project files.

---
+++ DemoIfElseAndCondnl/IfELseCondn/If.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoIfelseandSwithStatement.IfElseCondn
+{   
+    //IfElse statement And Conditional
+    public class IfElseCondn
+    {
+        public static void GetHour()
+        {
+            int hour;
+            Console.Write("Enter hour");
+            hour = Convert.ToInt32(Console.ReadLine());
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's Morning\n");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's Afternoon\n");
+            }
+            else
+            {
+                Console.WriteLine("It's Evening\n");
+            }
+        }
+        public static void GetCustomer()
+        {
+            bool isGoldCustomer = true;
+            float price = (isGoldCustomer) ? 19.95f: 29.95f ;
+            Console.WriteLine(price);
+        }
+    }
+}
---
+++ DemoIfElseAndCondnl/Program.cs
@@ -0,0 +1,33 @@
+﻿using DemoIfelseandSwithStatement.IfElseCondn;
+using System;
+namespace DemoIfandSwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choose;
+        A: Console.WriteLine("**************Enter Your Choice**************\n\n");
+            Console.WriteLine("1.Check Hour\t\t\t 2. Conditonal Statement");
+            choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    IfElseCondn.GetHour();
+                    goto A;
+
+                case 2:
+                    IfElseCondn.GetCustomer();
+                    goto A;
+                default:
+                    Console.WriteLine("\t\t!!!!!!!!!!!!!!Wrong Input!!!!!!!!!!!!!!\n");
+                    goto A;
+
+            }
+
+
+        }
+
+
+    }
+}
